b9a84bf0fecf63e1924aec2f44e61f54595f01d2
Eduardo Rocha <eduardor626@yahoo.com>
2020-02-06 11:20:39 -0800

    Setting up main PWM functions

--- Lab9/source/main.c
+++ Lab9/source/main.c
@@ -1,7 +1,7 @@
-/*	Author: eduardorocha
- *  Partner(s) Name: Eduardo Rocha
+/*	Author: Eduardo Rocha
+ *  Partner(s) Name: Arturo Alvarado
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab #9  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -11,6 +11,38 @@
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
+
+void set_PWM(double frequency){
+	static double current_frequency;
+
+	if(frequency != current_frequency){
+		if(!frequency) {TCCR3B &= 0x08;}
+		else{TCCR3B |= 0x03;}
+
+		if(frequency < 0.954){OCR3A = 0xFFFF;}
+
+		else if(frequency > 31250) {OCR3A = 0x0000;}
+
+		else {OCR3A = (short)(8000000 / (128*frequency))-1;}
+
+		TCNT3 = 0;
+		current_frequency = frequency;
+	}
+
+}
+
+void PWM_on(){
+	TCCR3A = (1 << COM3A0);
+	TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+	set_PWM(0);
+
+}
+
+void PWM_off(){
+	TCCR3A = 0x00;
+	TCCR3B = 0x00;
+
+}
 
 int main(void) {
     /* Insert DDR and PORT initializations */
